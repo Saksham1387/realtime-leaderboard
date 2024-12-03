42b75b4be6736380d731cd99aa4bc68aaace88a6
saksham56 <saksham1387@gmail.com>
2024-12-03 16:38:37 +0530

    build-fix

--- frontend/src/components/leaderboard.tsx
+++ frontend/src/components/leaderboard.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
